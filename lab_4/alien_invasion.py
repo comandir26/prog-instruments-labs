d7a94e0eb67187e8721afe954376e8a82c2db3c8
Vladislav <WWolkoW04@yandex.ru>
2024-11-26 03:53:43 +0400

    Initial commit

---
+++ lab_4/alien_invasion.py
@@ -0,0 +1,43 @@
+import pygame
+
+from pygame.sprite import Group
+
+import game_functions as gf
+
+from settings import Settings
+from ship import Ship
+from game_stats import GameStats
+from button import Button
+from scoreboard import Scoreboard
+
+
+def run_game():
+	"""Initializes the game and creates a screen object"""
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width,
+								   		 ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+	play_button = Button(screen, "Play")
+	stats = GameStats(ai_settings)
+	scoreboard = Scoreboard(screen, ai_settings, stats)
+	ship = Ship(screen, ai_settings)
+	bullets = Group()
+	aliens = Group()
+	stars = Group()
+	gf.create_stars(screen, ai_settings, stars)
+	gf.create_fleet(screen, ai_settings, aliens, ship)
+	while True:
+		gf.check_events(ai_settings, screen, ship, bullets, stats, play_button,
+				  		 aliens, scoreboard)
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(bullets, aliens, screen, ai_settings, ship, scoreboard, 
+					 			stats)
+			gf.update_aliens(aliens, ai_settings, ship, stats, screen, bullets, scoreboard)
+		gf.update_screen(ai_settings, screen, ship, bullets, aliens, stars, stats,
+				   			 play_button, scoreboard)
+
+if __name__ == '__main__':
+	run_game()
+ 
---
+++ lab_4/star.py
@@ -0,0 +1,23 @@
+import pygame
+
+from pygame.sprite import Sprite
+
+
+class Star(Sprite):
+	"""A class that implements the star"""
+
+	def __init__(self, screen):
+		"""Initializes the attributes of the star"""
+		super().__init__()
+		self.screen = screen
+		self.image = pygame.image.load("lab_1/images/starsmall.png")
+		self.rect = self.image.get_rect()
+		self.rect.x = self.rect.width
+		self.rect.y = self.rect.height
+		self.x = float(self.rect.x)
+		self.y = float(self.rect.y)
+		
+	def blitme(self):
+		"""Draws the star in the current position"""
+		self.screen.blit(self.image, self.rect)
+ 
